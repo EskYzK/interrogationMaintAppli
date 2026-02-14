dff311aeaa58af4dcc48ebe8c26915b1a5fcb3d7
EskYzK <alexkerber54190@gmail.com>
2026-02-14 09:43:43 +0100

    :sparkles: Création d'un dossier de tests

---
+++ tests/testTaskManager.ts
@@ -0,0 +1,8 @@
+import { TaskManager } from "../src/TaskManager.ts";
+import {Task} from "../src/TaskClass.ts"
+
+export class Tests{
+    public testAjout(ID:number,title:string,desc:string){
+        
+    }
+}
